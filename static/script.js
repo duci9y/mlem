d59e2af0482e1bf347c578a35e58424276ba23eb
Leland Wu <leland.wu4@gmail.com>
2018-12-05 12:34:52 -0500

    make curves smoother

--- static/script.js
+++ static/script.js
@@ -9,8 +9,11 @@ class Controller {
         this.ctx = this.fixedCanvas.getContext('2d')
         this.drawingCtx = this.updatesCanvas.getContext('2d')
 
-        this.ctx.lineWidth = 2
-        this.drawingCtx.lineWidth = 2
+        this.ctx.lineWidth = 10
+        this.drawingCtx.lineWidth = 10
+
+        this.ctx.lineJoin = this.ctx.lineCap = 'round'
+        this.drawingCtx.lineJoin = this.drawingCtx.lineCap = 'round'
 
         this.currX = 0
         this.currY = 0
@@ -18,8 +21,12 @@ class Controller {
         this.mouseHeld = false
         this.batcher = 0
 
+
+
         this.socket = io.connect('//' + document.domain + ':' + location.port)
         this.socket.binaryType = 'blob'
+
+        this.isDrawing, this.points = [ ]
 
         this.setupSocketHandlers()
         this.setupCanvasHandlers()
@@ -58,41 +65,68 @@ class Controller {
         img.src = data
     }
 
+    midPointBtw(p1, p2) {
+      return {
+        x: p1.x + (p2.x - p1.x) / 2,
+        y: p1.y + (p2.y - p1.y) / 2
+      };
+    }
+
     mouseDown(e) {
         this.currX = e.clientX - this.updatesCanvas.offsetLeft + $(document).scrollLeft()
         this.currY = e.clientY - this.updatesCanvas.offsetTop + $(document).scrollTop()
-        this.mouseHeld = true
+        // this.mouseHeld = true
 
-        this.ctx.beginPath()
-        this.ctx.moveTo(this.currX, this.currY)
-        this.drawingCtx.beginPath()
-        this.drawingCtx.moveTo(this.currX, this.currY)
+        // this.ctx.beginPath()
+        // this.ctx.moveTo(this.currX, this.currY)
+        // this.drawingCtx.beginPath()
+        // this.drawingCtx.moveTo(this.currX, this.currY)
+
+        this.isDrawing = true;
+        this.points.push({ x: this.currX, y: this.currY });
     }
 
     mouseMove(e) {
-        if (!this.mouseHeld) { return }
-
-        this.batcher++
+        if (!this.isDrawing) return;
 
         this.currX = e.clientX - this.updatesCanvas.offsetLeft + $(document).scrollLeft()
         this.currY = e.clientY - this.updatesCanvas.offsetTop + $(document).scrollTop()
+  
+        this.points.push({ x: this.currX, y: this.currY });
+          
+        var p1 = this.points[0];
+        var p2 = this.points[1];
+          
+        this.ctx.beginPath()
+        this.ctx.moveTo(p1.x, p1.y)
+        this.drawingCtx.beginPath()
+        this.drawingCtx.moveTo(p1.x, p1.y)
 
-        this.ctx.lineTo(this.currX, this.currY)
+        console.log(this.points);
+
+        for (var i = 1, len = this.points.length; i < len; i++) {
+            // we pick the point between pi+1 & pi+2 as the
+            // end point and p1 as our control point
+            var midPoint = this.midPointBtw(p1, p2);
+            this.ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+            this.drawingCtx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+            p1 = this.points[i];
+            p2 = this.points[i+1];
+        }
+        // Draw last line as a straight line while
+        // we wait for the next point to be able to calculate
+        // the bezier control point
+        this.ctx.lineTo(p1.x, p1.y)
         this.ctx.stroke()
-        this.drawingCtx.lineTo(this.currX, this.currY)
-        // stroke only one canvas, stroke the other on mouseup
-        // this.drawingCtx.stroke()
 
-        if (this.batcher < 25) { return }
-
-        this.batcher = 0
+        this.drawingCtx.lineTo(p1.x, p1.y)
 
         this.sendUpdates()
     }
 
     mouseUp(e) {
-        if (!this.mouseHeld) { return }
-        this.mouseHeld = false
+        this.isDrawing = false;
+        this.points.length = 0;
 
         this.sendUpdates()
     }
@@ -139,6 +173,8 @@ class Controller {
         dlink.setAttribute("download", "image.png")
 
     }
+
+
 }
 
 var ctrl = new Controller()
